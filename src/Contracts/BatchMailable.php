61a9de90831d1b34c1065bb1f0f1c9955fd4dc4e
Mateus Junges <mateus@junges.dev>
2022-12-18 17:03:39 -0300

    Add methods back to contract

--- src/Contracts/BatchMailable.php
+++ src/Contracts/BatchMailable.php
@@ -2,8 +2,6 @@
 
 namespace InteractionDesignFoundation\BatchMailer\Contracts;
 
-use InteractionDesignFoundation\BatchMailer\Mailables\Content;
-use InteractionDesignFoundation\BatchMailer\Mailables\Envelope;
 use InteractionDesignFoundation\BatchMailer\ValueObjects\Address;
 use InteractionDesignFoundation\BatchMailer\SentMessage;
 
@@ -12,10 +10,25 @@ interface BatchMailable
     /** Send the message using the given batch mailer.*/
     public function send(BatchMailer $batchMailer): ?SentMessage;
 
-   public function envelope(): Envelope;
+    /**
+     * Set the recipients of the message.
+     *
+     * @param array<int, Address> $addresses
+     */
+    public function cc(array $addresses): self;
 
-   /** Defines the content of the message. */
-   public function content(): Content;
+    /**
+     * Set the recipients of the message.
+     * @param array<int, Address> $addresses
+     */
+    public function bcc(array $addresses): self;
+
+    /**
+     * Set the recipients of the message.
+     *
+     * @param array<int, Address> $addresses
+     */
+    public function to(Address|array $addresses): self;
 
     /** Set the name of the mailer that should be used to send the message. */
     public function mailer(string $mailer): self;
